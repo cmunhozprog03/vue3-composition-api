57f5889b8c5dd779883989851102a29a0c8e77bd
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-04-13 14:22:47 -0300

    preloader

--- src/views/Todos/TodosView.vue
+++ src/views/Todos/TodosView.vue
@@ -1,5 +1,44 @@
 <template>
   <div>
     <h1>Lista de Tarefas</h1>
+    <div v-if="loading">
+      Carregando as tarefas...
+    </div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.identify">
+        {{ todo.title }}
+      </li>
+
+    </ul>
   </div>
 </template>
+
+<script>
+
+import TodoService  from '@/services/todos.service'
+import { onMounted, ref } from 'vue'
+
+export default {
+  name: 'TodosView',
+  setup() {
+
+    const todos = ref([])
+    const loading = ref(false)
+
+    onMounted(() => {
+
+      loading.value - true
+
+      TodoService.getAll()
+                .then(response =>todos.value = response.data.data)
+                .catch(error => console.log(error))
+                .finally(() => loading.value = false)
+    })
+
+    return {
+      todos,
+      loading
+    }
+  }
+}
+</script>
